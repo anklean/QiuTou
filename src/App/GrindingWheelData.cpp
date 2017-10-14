a9709586f183e9bf01064e9bf00d5fbc316e66a4
anklean <anklean@163.com>
2017-10-14 15:12:33 +0800

    提交遗漏的问题

--- src/App/GrindingWheelData.cpp
+++ src/App/GrindingWheelData.cpp
@@ -7,9 +7,9 @@ GrindingWheelData::GrindingWheelData()
 	 name=QLatin1String("1"); //刀具名称或者编号
 
 	//刀具类型==砂轮
-	 diameter = 4;//刀具的直径
-	 width=0.02;//刀具的宽度
-	 angle=20;//刀具的角度
+	 diameter = 0.8;//刀具的直径
+	 width=0.3;//刀具的宽度
+	 angle=90;//刀具的角度
 
 	direction=Base::Vector3d(0,-1,0);//刀具的方向
 }
